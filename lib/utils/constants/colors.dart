8fdf685c234308752b4cb77d888cabdb2f135f14
Akshya1204 <akshyakandula1204@gmail.com>
2024-06-11 23:48:18 +0530

    folders

---
+++ lib/utils/constants/colors.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+class Tcolors {
+  Tcolors._();
+
+//app basic color
+  static const Color Primarycolor = Color(0xffcaf0f8);
+  static const Color Secondarycolor = Color(0xff0077b6);
+  static const Color accent= Color(0xfff1faee);
+
+//Text colors
+  static const Color textPrimary = Color(0xff333333);
+  static const Color textSecondary = Color(0xff495057);
+  static const Color textwhite = Colors.white;
+
+//Background colors
+   static const Color light = Colors.white;
+   static const Color dark = Color(0xff272727);
+   static const Color Primarybackground = Color(0xffe9ecef);
+
+//Backgroundcontainer
+  static const Color lightcontainer = Color.fromARGB(255, 234, 242, 244);
+  static const Color darkcontainer = Color.fromARGB(255, 20, 20, 20);
+
+//button color
+  static const Color primarybutton = Color(0xff0077b6);
+  static const Color secondarybutton =Color(0xff495057);
+  static const Color buttondiabled = Color.fromARGB(255, 250, 252, 252);
+ 
+//border color
+  static const Color Primaryborder = Color(0xfff2e9e4);
+  static const Color secondaryborder= Color(0xfff8f9fa);
+
+//validation
+  static const Color error = Color(0xfff94144);
+  static const Color success = Color(0xff57cc99);
+  static const Color warning = Color(0xffee964b);
+  static const Color info = Color(0xff4895ef);
+
+//neutral shades
+  static const Color black = Color(0xffcaf0f8);
+  static const Color darkergrey= Color(0xffcaf0f8)
+    static const Color darkergrey= Color(0xffcaf0f8);
+  static const Color darkergrey= Color(0xffcaf0f8);
+  static const Color Primarycolor = Color(0xffcaf0f8);
+  static const Color Primarycolor = Color(0xffcaf0f8);
+}
