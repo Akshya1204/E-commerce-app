f0d43357f0f6e7417c79a164b974b9d62e01b1b5
Akshya1204 <akshyakandula1204@gmail.com>
2024-06-13 21:55:48 +0530

    assests

---
+++ lib/utils/constants/image_strings.dart
@@ -0,0 +1,6 @@
+// import 'package:flutter/material.dart';
+
+class Timages {
+  static const String google = "assets/logos/search.png";
+  static const String facebook = "assets/logos/facebook.png";
+}
